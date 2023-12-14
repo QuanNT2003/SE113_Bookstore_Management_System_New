0efa05b3974fef3ebbc23db9605ba27ea3bbadd5
Lê Đăng Thương <ledangthuongsp@gmail.com>
2023-12-14 09:12:31 +0700

    save jest

---
+++ jest.setup.js
@@ -0,0 +1,10 @@
+window.matchMedia = window.matchMedia || function() {
+    return {
+        matches: false,
+        addListener: function() {},
+        removeListener: function() {},
+        addEventListener: function() {},
+        removeEventListener: function() {},
+        dispatchEvent: function() {},
+    };
+};
