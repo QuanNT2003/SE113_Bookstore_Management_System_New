cfa5c751e5d1a6a6070d371be0c07d697a5e1245
Lê Đăng Thương <ledangthuongsp@gmail.com>
2023-12-24 13:20:25 +0700

    test login

--- frontend/src/pages/Login/Login.test.js
+++ frontend/src/pages/Login/Login.test.js
@@ -1,32 +1,49 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Login from '../Login/index'; // Điều chỉnh đường dẫn đến component Login của bạn
-
+import Login from '../Login/index'; // Adjust the path to your Login component
 describe('Login Component Tests', () => {
+
+    test('login successful with correct credentials', async () => {
+        render(<Login />);
+        fireEvent.change(screen.getByPlaceholderText('Số điện thoại hoặc email'), { target: { value: 'correct@email.com' } });
+        fireEvent.change(screen.getByPlaceholderText('Mật khẩu'), { target: { value: 'correctPassword' } });
+        fireEvent.click(screen.getByRole('button', { name: 'ĐĂNG NHẬP' }));
+
+        // Assert successful login behavior here (e.g., redirecting, displaying a success message)
+    });
+
+    test('shows error on wrong password', async () => {
+        render(<Login />);
+        fireEvent.change(screen.getByPlaceholderText('Số điện thoại hoặc email'), { target: { value: 'user@email.com' } });
+        fireEvent.change(screen.getByPlaceholderText('Mật khẩu'), { target: { value: 'wrongPassword' } });
+        fireEvent.click(screen.getByRole('button', { name: 'ĐĂNG NHẬP' }));
+
+        // Assert error behavior here (e.g., displaying an error message)
+    });
     
     test('renders Login component', () => {
-        render(React.createElement(Login));
+        render(<Login />);
         expect(screen.getByText('Đăng nhập vào cửa hàng của bạn')).toBeInTheDocument();
     });
 
     test('has input for phone number or email', () => {
-        render(React.createElement(Login));
+        render(<Login />);
         expect(screen.getByPlaceholderText('Số điện thoại hoặc email')).toBeInTheDocument();
     });
 
     test('has input for password', () => {
-        render(React.createElement(Login));
+        render(<Login />);
         expect(screen.getByPlaceholderText('Mật khẩu')).toBeInTheDocument();
     });
 
     test('has a login button', () => {
-        render(React.createElement(Login));
+        render(<Login />);
         expect(screen.getByRole('button', { name: 'ĐĂNG NHẬP' })).toBeInTheDocument();
     });
 
     test('has a link for forgotten password', () => {
-        render(React.createElement(Login));
+        render(<Login />);
         expect(screen.getByText('Quên mật khẩu')).toBeInTheDocument();
     });
 });
