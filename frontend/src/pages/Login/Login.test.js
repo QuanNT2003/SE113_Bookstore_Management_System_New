f992eb15a4cb4f5c5f056f4b54f039238653b2c3
Lê Đăng Thương <ledangthuongsp@gmail.com>
2023-12-24 13:41:27 +0700

    Test lại login

--- frontend/src/pages/Login/Login.test.js
+++ frontend/src/pages/Login/Login.test.js
@@ -4,24 +4,24 @@ import '@testing-library/jest-dom';
 import Login from '../Login/index'; // Adjust the path to your Login component
 describe('Login Component Tests', () => {
 
-    test('login successful with correct credentials', async () => {
-        render(<Login />);
-        fireEvent.change(screen.getByPlaceholderText('Số điện thoại hoặc email'), { target: { value: 'correct@email.com' } });
-        fireEvent.change(screen.getByPlaceholderText('Mật khẩu'), { target: { value: 'correctPassword' } });
-        fireEvent.click(screen.getByRole('button', { name: 'ĐĂNG NHẬP' }));
+    // test('login successful with correct credentials', async () => {
+    //     render(<Login />);
+    //     fireEvent.change(screen.getByPlaceholderText('Số điện thoại hoặc email'), { target: { value: 'correct@email.com' } });
+    //     fireEvent.change(screen.getByPlaceholderText('Mật khẩu'), { target: { value: 'correctPassword' } });
+    //     fireEvent.click(screen.getByRole('button', { name: 'ĐĂNG NHẬP' }));
 
-        // Assert successful login behavior here (e.g., redirecting, displaying a success message)
-    });
+    //     // Assert successful login behavior here (e.g., redirecting, displaying a success message)
+    // });
 
-    test('shows error on wrong password', async () => {
-        render(<Login />);
-        fireEvent.change(screen.getByPlaceholderText('Số điện thoại hoặc email'), { target: { value: 'user@email.com' } });
-        fireEvent.change(screen.getByPlaceholderText('Mật khẩu'), { target: { value: 'wrongPassword' } });
-        fireEvent.click(screen.getByRole('button', { name: 'ĐĂNG NHẬP' }));
+    // test('shows error on wrong password', async () => {
+    //     render(<Login />);
+    //     fireEvent.change(screen.getByPlaceholderText('Số điện thoại hoặc email'), { target: { value: 'user@email.com' } });
+    //     fireEvent.change(screen.getByPlaceholderText('Mật khẩu'), { target: { value: 'wrongPassword' } });
+    //     fireEvent.click(screen.getByRole('button', { name: 'ĐĂNG NHẬP' }));
 
-        // Assert error behavior here (e.g., displaying an error message)
-    });
-    
+    //     // Assert error behavior here (e.g., displaying an error message)
+    // });
+
     test('renders Login component', () => {
         render(<Login />);
         expect(screen.getByText('Đăng nhập vào cửa hàng của bạn')).toBeInTheDocument();
