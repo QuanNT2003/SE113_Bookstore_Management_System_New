8ff8a891c153d6495a271ae41c73792767e6e3eb
Lê Đăng Thương <ledangthuongsp@gmail.com>
2023-12-24 12:55:11 +0700

    Test lại login

--- frontend/src/pages/Login/Login.test.js
+++ frontend/src/pages/Login/Login.test.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Login from '../Login/index'; // Adjust the path to your Login component
+import Login from '../Login/index'; // Điều chỉnh đường dẫn đến component Login của bạn
+
 describe('Login Component Tests', () => {
     
     test('renders Login component', () => {
-        render(<Login />);
+        render(React.createElement(Login));
         expect(screen.getByText('Đăng nhập vào cửa hàng của bạn')).toBeInTheDocument();
     });
 
     test('has input for phone number or email', () => {
-        render(<Login />);
+        render(React.createElement(Login));
         expect(screen.getByPlaceholderText('Số điện thoại hoặc email')).toBeInTheDocument();
     });
 
     test('has input for password', () => {
-        render(<Login />);
+        render(React.createElement(Login));
         expect(screen.getByPlaceholderText('Mật khẩu')).toBeInTheDocument();
     });
 
     test('has a login button', () => {
-        render(<Login />);
+        render(React.createElement(Login));
         expect(screen.getByRole('button', { name: 'ĐĂNG NHẬP' })).toBeInTheDocument();
     });
 
     test('has a link for forgotten password', () => {
-        render(<Login />);
+        render(React.createElement(Login));
         expect(screen.getByText('Quên mật khẩu')).toBeInTheDocument();
     });
 });
