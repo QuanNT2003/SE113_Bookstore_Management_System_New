5d6c3a55b0291351b8b8e01700d4074385e05786
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-01-05 11:39:50 +0700

    tạo lại

--- frontend/stryker.conf.js
+++ frontend/stryker.conf.js
@@ -6,7 +6,7 @@
 module.exports = {
     mutate: [
       'src/pages/**/*.js', // This will include all JS files in the pages directory
-      '!src/pages/**/*.test.js', // This will exclude test files within the pages directory
+      '!src/pages/**/*.scss', // This will exclude test files within the pages directory
     ],
     testFramework: 'mocha', // Change this to your actual test framework if it's not mocha
     testRunner: 'mocha', // Change this to your actual test runner if it's not mocha
