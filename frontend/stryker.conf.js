263bfb5a3544e2d5ce219421291830e0be214eb5
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-01-05 11:41:11 +0700

    tạo lại

--- frontend/stryker.conf.js
+++ frontend/stryker.conf.js
@@ -8,8 +8,6 @@ module.exports = {
       'src/pages/**/*.js', // This will include all JS files in the pages directory
       '!src/pages/**/*.scss', // This will exclude test files within the pages directory
     ],
-    testFramework: 'mocha', // Change this to your actual test framework if it's not mocha
-    testRunner: 'mocha', // Change this to your actual test runner if it's not mocha
     reporters: ['progress', 'clear-text', 'html'],
     coverageAnalysis: 'perTest',
     // Add any other configuration options needed for Stryker here
