08a7d2b1dd4161d7df156e702d223072c5bf4198
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-01-05 11:38:29 +0700

    tạo lại

--- frontend/stryker.conf.js
+++ frontend/stryker.conf.js
@@ -1,12 +1,17 @@
-module.exports = function(config){
-    config.set({
-      mutate: [
-        'src/pages/**/*.js',
-        '!src/pages/index.js'
-      ],
-      testFramework: 'mocha',
-      testRunner: 'mocha',
-      reporters: ['progress', 'clear-text', 'html'],
-      coverageAnalysis: 'perTest'
-    });
-  }
+// stryker.conf.js
+
+/**
+  * @type {import('@stryker-mutator/api/core').StrykerOptions}
+  */
+module.exports = {
+    mutate: [
+      'src/pages/**/*.js', // This will include all JS files in the pages directory
+      '!src/pages/**/*.test.js', // This will exclude test files within the pages directory
+    ],
+    testFramework: 'mocha', // Change this to your actual test framework if it's not mocha
+    testRunner: 'mocha', // Change this to your actual test runner if it's not mocha
+    reporters: ['progress', 'clear-text', 'html'],
+    coverageAnalysis: 'perTest',
+    // Add any other configuration options needed for Stryker here
+  };
+  
